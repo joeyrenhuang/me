f18fd43d64d1f354323ce07d775aee0b2aa62e2a
joey <joey.huang@tech-core.org>
2016-10-19 19:07:36 +0800

    menu activs

--- src/main.js
+++ src/main.js
@@ -69,6 +69,15 @@ document.querySelector('.fa.next').addEventListener('click', function(){
   setSlideContent(1)
 })
 
+document.querySelector('header nav a.collapse').addEventListener('click', function(){
+  if (this.parentNode.className.indexOf('open') != -1){
+    this.parentNode.className = this.parentNode.className.replace('open', '')
+    this.className = 'collapse'
+  } else {
+    this.parentNode.className = 'open'
+    this.className += ' open'
+  }
+})
 
 
 
